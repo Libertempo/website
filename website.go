6e812698066b19eb26b96cd133b435163bb3b6a2
Prytoegrian <prytoegrian@protonmail.com>
2018-03-18 17:56:09 +0100

    First usable page \o/

--- website.go
+++ website.go
@@ -12,6 +12,7 @@ var addr = flag.String("addr", ":1718", "http service address") // Q=17, R=18
 
 func main() {
 	http.Handle("/", http.HandlerFunc(index))
+	http.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir("public"))))
 	err := http.ListenAndServe(*addr, nil)
 	if err != nil {
 		log.Fatal("ListenAndServe:", err)
@@ -21,7 +22,7 @@ func main() {
 func index(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "text/html ; charset=utf-8")
 	wd, _ := os.Getwd()
-	file := wd + "/index.html"
+	file := wd + "/templates/index.html"
 	t, err := template.New("root").ParseFiles(file)
 	if err != nil {
 		log.Fatal("Template error :", err)
